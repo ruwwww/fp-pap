120997183472f70144fb2f1411a67f625e06f105
ascndia <fixgmc@gmail.com>
2026-06-17 20:46:41 +0700

    chore: remove legacy multi-model code, keep single-template setup

--- shared/__init__.py
+++
@@ -1,3 +0,0 @@
-from .load_data import load_train, load_test, temporal_split
-from .metrics import rmse, mape, mae, r2, smape, evaluate_all
-from .plotting import plot_actual_vs_predicted, plot_forecast
